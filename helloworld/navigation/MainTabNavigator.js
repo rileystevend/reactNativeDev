c732b13682e2bcbd52c4f3f7e663f4feee5ccc14
Steven Riley <rileystevend@gmail.com>
2018-10-04 09:01:53 -0500

    working with icons

--- helloworld/navigation/MainTabNavigator.js
+++ helloworld/navigation/MainTabNavigator.js
@@ -30,7 +30,7 @@ const LinksStack = createStackNavigator({
 });
 
 LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
+  tabBarLabel: 'My Cards',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
